a20c8a4aa87e39bfd32becc7ba77f47975394c40
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-23 17:36:04 +0300

    Update home page UI with "Ask a Question" button and question list layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+import {ROUTES} from "@/refs/routes";
+import {Button} from "@/components/ui/button";
+
 function PageHome() {
   return (
     <>
-      <h1 className="h1-bold">Welcome</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          asChild
+          className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3"
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 }
