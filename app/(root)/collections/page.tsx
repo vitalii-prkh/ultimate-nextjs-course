64919d6aa0d4a4cfa5d8af6c43592c76d1737ae9
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-29 16:39:35 +0300

    Add `getSavedQuestions` action and integrate saved questions into `PageCollections`
    - Implement `getSavedQuestions` action to fetch saved questions with pagination, search, and filters.
    - Extend `PageCollections` to utilize `getSavedQuestions` for dynamic rendering of saved questions.
    - Add `LocalSearch`, `HomeTags`, and `DataRenderer` components for search and filter functionality in the collections page.
    - Enhance `Collection` model with improved typings (`TCollectionJSON`, `TQuestionJSON`) for better data handling.

--- app/(root)/collections/page.tsx
+++ app/(root)/collections/page.tsx
@@ -1,5 +1,62 @@
-function PageCollections() {
-  return <div>PageCollections</div>;
+import {ROUTES} from "@/refs/routes";
+import {EMPTY_QUESTION} from "@/refs/states";
+import {COLLECTION_FILTERS} from "@/refs/filters";
+import {getSavedQuestions} from "@/lib/actions/collection.actions";
+import {LocalSearch} from "@/components/search/LocalSearch";
+import {HomeTags} from "@/components/filters/HomeTags";
+import {DataRenderer} from "@/components/DataRenderer";
+import {CardQuestion} from "@/components/cards/CardQuestion";
+
+type PageHomeProps = {
+  searchParams: Promise<{
+    page?: string;
+    pageSize?: string;
+    query?: string;
+    tag?: string;
+  }>;
+};
+
+async function PageCollections(props: PageHomeProps) {
+  const {page, pageSize, query, tag} = await props.searchParams;
+  const {success, data, error} = await getSavedQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query: query || "",
+    filter: tag || "",
+  });
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      </section>
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.COLLECTIONS}
+          image="/icons/search.svg"
+          placeholder="Search questions..."
+          className="flex-1"
+        />
+      </section>
+      <HomeTags options={COLLECTION_FILTERS} />
+      <DataRenderer
+        success={success}
+        error={error}
+        data={data?.data}
+        empty={EMPTY_QUESTION}
+        render={(collections) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {collections.map((collection) => (
+              <CardQuestion
+                key={collection.question._id}
+                data={collection.question}
+              />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 }
 
 export default PageCollections;
--- lib/actions/collection.actions.ts
+++ lib/actions/collection.actions.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import {PipelineStage, Types} from "mongoose";
 import {revalidatePath} from "next/cache";
 import {z} from "zod";
-import Question from "@/db/question.model";
-import Collection from "@/db/collection.model";
+import Question, {TQuestionJSON} from "@/db/question.model";
+import Collection, {TCollectionJSON} from "@/db/collection.model";
+import {TUserJSON} from "@/db/user.model";
+import {TTagJSON} from "@/db/tag.model";
 import {ROUTES} from "@/refs/routes";
-import {schemaCollectionBase} from "@/lib/validations";
+import {schemaCollectionBase, schemaSearchParams} from "@/lib/validations";
 import {action} from "@/lib/handlers/action";
 import {handleError} from "@/lib/handlers/error";
 import {buildPath} from "@/lib/path/buildPath";
@@ -108,6 +111,119 @@ export async function hasSaveQuestion(
       success: true,
       data: {
         saved: Boolean(collection),
+      },
+    };
+  } catch (error) {
+    return handleError(error, "server");
+  }
+}
+
+type TGetSavedQuestionsParams = z.infer<typeof schemaSearchParams>;
+
+type TGetSavedQuestionsData = {
+  isNext: boolean;
+  data: TCollectionInList[];
+};
+
+type TCollectionInList = Omit<TCollectionJSON, "question"> & {
+  question: Omit<TQuestionJSON, "author" | "tags"> & {
+    author: TUserJSON;
+    tags: TTagJSON[];
+  };
+};
+
+export async function getSavedQuestions(
+  params: TGetSavedQuestionsParams,
+): Promise<SuccessResponse<TGetSavedQuestionsData> | FailureResponse> {
+  const validationResult = await action({
+    params,
+    schema: schemaSearchParams,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult, "server");
+  }
+
+  const userId = validationResult.session?.user?.id;
+  const {page = 1, pageSize = 10, query, filter} = validationResult.params!;
+  const limit = Number(pageSize);
+  const skip = (Number(page) - 1) * limit;
+  const sortOptions: Record<string, Record<string, 1 | -1>> = {
+    mostrecent: {"question.createdAt": -1},
+    oldest: {"question.createdAt": 1},
+    mostvoted: {"question.upvotes": -1},
+    mostviewed: {"question.views": -1},
+    mostanswered: {"question.answers": -1},
+  };
+  const sortCriteria =
+    sortOptions[filter as keyof typeof sortOptions] || sortOptions.mostrecent;
+
+  try {
+    const pipeline: PipelineStage[] = [
+      {
+        $match: {
+          author: new Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "questions",
+          localField: "question",
+          foreignField: "_id",
+          as: "question",
+        },
+      },
+      {
+        $unwind: "$question",
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "question.author",
+          foreignField: "_id",
+          as: "question.author",
+        },
+      },
+      {
+        $unwind: "$question.author",
+      },
+      {
+        $lookup: {
+          from: "tags",
+          localField: "question.tags",
+          foreignField: "_id",
+          as: "question.tags",
+        },
+      },
+    ];
+
+    if (query) {
+      pipeline.push({
+        $match: {
+          $or: [
+            {"question.title": {$regex: query, $options: "i"}},
+            {"question.content": {$regex: query, $options: "i"}},
+          ],
+        },
+      });
+    }
+
+    const [totalCount] = await Collection.aggregate([
+      ...pipeline,
+      {$count: "count"},
+    ]);
+
+    pipeline.push({$sort: sortCriteria}, {$skip: skip}, {$limit: limit});
+    pipeline.push({$project: {question: 1, author: 1}});
+
+    const questions = await Collection.aggregate(pipeline);
+
+    return {
+      success: true,
+      data: {
+        data: JSON.parse(JSON.stringify(questions)),
+        isNext: totalCount.count > skip + questions.length,
       },
     };
   } catch (error) {
